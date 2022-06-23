2c097dd15810900f6b3caf4e92c1c478a2c1eb04
MsVoxxie <odysseylove222@gmail.com>
2022-06-23 19:01:50 -0400

    Return color with #Hex

--- src/routes/canvas.js
+++ src/routes/canvas.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 router.get('/palette', async (req, res) => {
 	const response = await axios.post('http://www.colourlovers.com/api/palettes/random?format=json');
 	const data = response.data[0];
-	const colors = data.colors;
+	const colors = data.colors.map((c) => `#${c}`);
 
 	res.send(colors);
 });
