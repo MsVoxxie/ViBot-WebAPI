415b103452f4cf75721098e7505cd3a61a079cd7
MsVoxxie <odysseylove222@gmail.com>
2022-07-18 22:21:00 -0400

    Gamestatus watcher

--- src/routes/gamestatus.js
+++ src/routes/gamestatus.js
@@ -1,8 +1,21 @@
 const { sendWebhook } = require('../functions/webHooks');
 const router = require('express').Router();
 const Gamedig = require('gamedig');
+require('dotenv').config();
 
-let oldPlayerCount = null;
+//Globals
+const servers = {
+	ATM7: {
+		PLAYER_COUNT: 0,
+		ONLINE: false,
+		STARTING: true,
+	},
+	KF2: {
+		PLAYER_COUNT: 0,
+		ONLINE: false,
+		STARTING: true,
+	},
+};
 
 // Minecraft / ATM7
 router.get('/atm7', async (req, res) => {
@@ -14,33 +27,53 @@ router.get('/atm7', async (req, res) => {
 			port: 25565,
 		});
 
-		//Set the server status to online
-		if (oldPlayerCount !== Query.players.length) {
-			const webHookData = {
+		//Server is online! Lets set stats
+		servers.ATM7.ONLINE = true;
+		(servers.ATM7.STARTING = false), (servers.ATM7.PLAYER_COUNT = Query.players.length);
+
+		//Check that the server was previously offline or the player count changed
+		if (!servers.ATM7.ONLINE || Query.players.length !== servers.ATM7.players) {
+			//Set description formatting
+			const currentPlayers = `\`\`\`Player Count: ${Query.players.length}/${Query.maxplayers}\`\`\``;
+			const playerList = `${Query.players.length > 0 ? `\`\`\`Players: ${Query.players.map((player) => (player.name.length > 0 ? player.name : '<Unknown Player>')).join(', ')}\`\`\`` : ''}`;
+			const serverMOTD = `${Query.name ? `\n\`\`\`css\nMOTD: ${Query.name}\`\`\`` : ''}`;
+
+			// Send the webhook
+			await sendWebhook({
 				title: 'ATM7 Server - Online',
-				description: `Players: ${Query.players.length}/${Query.maxplayers}`,
-				URL: 'https://discord.com/api/webhooks/997607208504275014/itjVBTcjhHcWyo6Jlk5Q-z8fhm9lxDqf6v-NOnuukDU5kqRZsk2hBK5NC6RWux7VNezo',
-			};
-
-			if (Query.players.length > 0) {
-				webHookData.fieldName = 'Players';
-				webHookData.fieldValue = `\`\`\`${Query.players.map((player) => player.name).join(', ')}\`\`\``;
-			}
-
-			sendWebhook(webHookData);
-			oldPlayerCount = Query.players.length;
-			res.json({ online: true });
-		}
-	} catch (error) {
-		//Set the server status to offline
-		if (oldPlayerCount !== oldPlayerCount) {
-			sendWebhook({
-				title: 'ATM7 Server - Offline',
-				URL: 'https://discord.com/api/webhooks/997607208504275014/itjVBTcjhHcWyo6Jlk5Q-z8fhm9lxDqf6v-NOnuukDU5kqRZsk2hBK5NC6RWux7VNezo',
+				description: `${serverMOTD}${currentPlayers}${playerList}`,
+				URL: process.env.ATM7_WEBHOOK,
 			});
-			oldPlayerCount = null;
-			res.json({ online: false });
+
+			//Update the player count
+			servers.ATM7.PLAYER_COUNT = Query.players.length;
 		}
+
+		//Send the response
+		res.send({ status: 'online', players: Query.players.length });
+		console.log('[ATM7] Requested', servers.ATM7);
+	} catch (error) {
+		//Server is offline!
+		servers.ATM7.ONLINE = false;
+		(servers.ATM7.STARTING = false), (servers.ATM7.PLAYER_COUNT = 0);
+
+		//Check that the server was previously online
+		if (servers.ATM7.ONLINE || servers.ATM7.STARTING) {
+			// Send the webhook
+			await sendWebhook({
+				title: 'ATM7 Server - Offline',
+				URL: process.env.ATM7_WEBHOOK,
+			});
+
+			console.log(`ATM7 Server - Offline`);
+
+			//Update the player count
+			servers.ATM7.PLAYER_COUNT = 0;
+		}
+
+		//Send the response
+		res.send({ status: 'offline', players: 0 });
+		console.log('[ATM7] Requested', servers.ATM7);
 	}
 });
 
